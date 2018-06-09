8516be58587fcf291b99b2f22b3a75f8baed1401
Gabriel Mule <gabemule@gmail.com>
2018-06-09 01:17:46 -0300

    Including detect adblocker screen to remove and removing on load to avoid clicking in extension

--- unblock.js
+++ unblock.js
@@ -1,15 +1,30 @@
 // this is the code which will be injected into a given page...
 
-(function() {
+// @TODO - improve functions and split into files
+function triggerUnblock() {
+	if (document.getElementById('detecta-adblock')) {
+		document.getElementById('detecta-adblock').remove();
+	}
 
 	if (document.getElementById('barreiraRegister')) {
 		document.getElementById('barreiraRegister').remove();
 	}
-	
+
 	if (document.getElementById('barreiraRegisterExclusiva')) {
 		document.getElementById('barreiraRegisterExclusiva').remove();
 	}
-	
-	document.getElementsByTagName("BODY")[0].style.overflow = "auto";
 
-})();
+	if (document.getElementsByTagName("BODY")[0].style.overflow !== "auto") {
+		document.getElementsByTagName("BODY")[0].style.overflow = "auto";
+	}
+};
+
+// document ready
+(function() {
+	(document.attachEvent ? document.readyState === "complete" : document.readyState !== "loading")
+		? triggerUnblock()
+		: document.addEventListener('DOMContentLoaded', triggerUnblock);
+}());
+
+// window load
+window.onload = function(){ triggerUnblock() };
